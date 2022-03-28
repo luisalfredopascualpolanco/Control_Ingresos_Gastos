25e02a1ddedf232fcc03a3d3f2b63f450e0465e4
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2022-03-27 19:32:56 -0700

    version1.0

---
+++ ControlIngresosGastos/ControlIngresosGastos/Data/AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ControlIngresosGastos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlIngresosGastos.Data
+{
+    public class AplicationDbContext:DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base (options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<IngresoGasto> IngresoGasto { get; set; }
+    }
+}
---
+++ ControlIngresosGastos/ControlIngresosGastos/Models/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlIngresosGastos.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(120)]
+        [Display(Name ="Nombre de Categoria")]
+        public string NombreCategoria { get; set; }
+
+        [Required]
+        [MaxLength(2)]
+        [Display(Name ="Tipo")]
+        public string tipo { get; set; } //IN ingreso GA Gasto
+
+        [Required]
+        public bool Estado { get; set; }
+    }
+}
